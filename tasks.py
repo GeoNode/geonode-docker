96134a6514af2c2bc80cd9a777e1b8b2f51f5e51
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-01-13 19:36:23 +0100

    Fix missing input value for hostname

--- tasks.py
+++ tasks.py
@@ -13,7 +13,7 @@ BOOTSTRAP_IMAGE_CHEIP = 'codenvy/che-ip:nightly'
 @task
 def waitfordbs(ctx):
     print "**************************databases*******************************"
-    ctx.run("/usr/bin/wait-for-databases", pty=True)
+    ctx.run("/usr/bin/wait-for-databases {0}".format('db'), pty=True)
 
 
 @task
