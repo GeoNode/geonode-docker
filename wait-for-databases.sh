1466ab54c24a99d880cff7307ce6aecdc51e8ee4
afabiani <alessio.fabiani@geo-solutions.it>
2020-11-16 14:32:34 +0100

    Revert - Fix: invalid option 'pager'

--- wait-for-databases.sh
+++ wait-for-databases.sh
@@ -5,17 +5,17 @@ set -e
 host="$1"
 shift
 
-until PGPASSWORD=${POSTGRES_PASSWORD} psql -h "$host" -U ${POSTGRES_USER} -c '\l'; do
+until PGPASSWORD=${POSTGRES_PASSWORD} psql -h "$host" -U ${POSTGRES_USER} -P "pager=off" -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
-until PGPASSWORD=${GEONODE_DATABASE_PASSWORD} psql -h "$host" -U ${GEONODE_DATABASE} -d ${GEONODE_DATABASE} -c '\l'; do
+until PGPASSWORD=${GEONODE_DATABASE_PASSWORD} psql -h "$host" -U ${GEONODE_DATABASE} -d ${GEONODE_DATABASE} -P "pager=off" -c '\l'; do
   >&2 echo "${GEONODE_DATABASE} is unavailable - sleeping"
   sleep 1
 done
 
-until PGPASSWORD=${GEONODE_GEODATABASE_PASSWORD} psql -h "$host" -U ${GEONODE_GEODATABASE} -d ${GEONODE_GEODATABASE} -c '\l'; do
+until PGPASSWORD=${GEONODE_GEODATABASE_PASSWORD} psql -h "$host" -U ${GEONODE_GEODATABASE} -d ${GEONODE_GEODATABASE} -P "pager=off" -c '\l'; do
   >&2 echo "${GEONODE_GEODATABASE} is unavailable - sleeping"
   sleep 1
 done
